1e115dc4c857793bd02cc7238a8c8614ce13cd86
Hirun Dinhas <hirundinhas91@gmail.com>
2024-06-21 13:05:36 +0530

    Creating Expanse and Income Forms

--- lib/screens/add_new_screen.dart
+++ lib/screens/add_new_screen.dart
@@ -1,5 +1,8 @@
 import 'package:expenz_app/constants/colors.dart';
 import 'package:expenz_app/constants/constants.dart';
+import 'package:expenz_app/models/expenses_model.dart';
+import 'package:expenz_app/models/income_model.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class AddNewPage extends StatefulWidget {
@@ -12,87 +15,250 @@ class AddNewPage extends StatefulWidget {
 class _AddNewPageState extends State<AddNewPage> {
   //state to track expnse or income
   int _selectedMethod = 0;
+  ExpenseCategory _expenseCategory = ExpenseCategory.food;
+  IncomeCategory _incomeCategory = IncomeCategory.freelance;
+
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _amountController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _titleController.dispose();
+    _descriptionController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: _selectedMethod == 0 ? kRed : kGreen,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: kDefalutPadding),
+      body: SafeArea(
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.symmetric(vertical: kDefalutPadding),
             child: Stack(
               children: [
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 50,
-                    ),
-                    Container(
-                      height: MediaQuery.of(context).size.height * 0.07,
-                      decoration: BoxDecoration(
-                        color: kWhite,
-                        borderRadius: BorderRadius.circular(100),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _selectedMethod = 0;
-                              });
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: _selectedMethod == 0 ? kRed : kWhite,
-                                borderRadius: BorderRadius.circular(100),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 55, vertical: 10),
-                                child: Text(
-                                  "Expense",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color:
-                                        _selectedMethod == 0 ? kWhite : kBlack,
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: kDefalutPadding),
+                  child: Column(
+                    children: [
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.07,
+                        decoration: BoxDecoration(
+                          color: kWhite,
+                          borderRadius: BorderRadius.circular(100),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _selectedMethod = 0;
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: _selectedMethod == 0 ? kRed : kWhite,
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 55, vertical: 10),
+                                  child: Text(
+                                    "Expense",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: _selectedMethod == 0
+                                          ? kWhite
+                                          : kBlack,
+                                    ),
                                   ),
                                 ),
                               ),
                             ),
-                          ),
-                          GestureDetector(
-                            onTap: () {
-                              setState(() {
-                                _selectedMethod = 1;
-                              });
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: _selectedMethod == 1 ? kGreen : kWhite,
-                                borderRadius: BorderRadius.circular(100),
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 55, vertical: 10),
-                                child: Text(
-                                  "Income",
-                                  style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                    color:
-                                        _selectedMethod == 1 ? kWhite : kBlack,
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  _selectedMethod = 1;
+                                });
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: _selectedMethod == 1 ? kGreen : kWhite,
+                                  borderRadius: BorderRadius.circular(100),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 55, vertical: 10),
+                                  child: Text(
+                                    "Income",
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: _selectedMethod == 1
+                                          ? kWhite
+                                          : kBlack,
+                                    ),
                                   ),
                                 ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.129,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "How much?",
+                          style: TextStyle(
+                            color: kLightGrey.withOpacity(0.8),
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextField(
+                          style: TextStyle(
+                            fontSize: 64,
+                            color: kWhite,
+                            fontWeight: FontWeight.bold,
+                          ),
+                          decoration: InputDecoration(
+                            hintText: "0",
+                            hintStyle: TextStyle(
+                              fontSize: 64,
+                              color: kWhite,
+                              fontWeight: FontWeight.bold,
+                            ),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                //user data form
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  margin: EdgeInsets.only(
+                      top: MediaQuery.of(context).size.height * 0.3),
+                  decoration: BoxDecoration(
+                    color: kWhite,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(32),
+                      topRight: Radius.circular(32),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Form(
+                      child: Column(
+                        children: [
+                          //drop down menu
+                          DropdownButtonFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: kDefalutPadding,
+                                horizontal: 20,
+                              ),
+                            ),
+                            items: _selectedMethod == 0
+                                ? ExpenseCategory.values.map((category) {
+                                    return DropdownMenuItem(
+                                      child: Text(category.name),
+                                      value: category,
+                                    );
+                                  }).toList()
+                                : IncomeCategory.values.map((category) {
+                                    return DropdownMenuItem(
+                                      child: Text(category.name),
+                                      value: category,
+                                    );
+                                  }).toList(),
+                            value: _selectedMethod == 0
+                                ? _expenseCategory
+                                : _incomeCategory,
+                            onChanged: (value) {
+                              setState(() {
+                                _selectedMethod == 0
+                                    ? _expenseCategory =
+                                        value as ExpenseCategory
+                                    : _incomeCategory = value as IncomeCategory;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: _titleController,
+                            decoration: InputDecoration(
+                              hintText: "Title",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: kDefalutPadding,
+                                horizontal: 20,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: _descriptionController,
+                            decoration: InputDecoration(
+                              hintText: "Description",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: kDefalutPadding,
+                                horizontal: 20,
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            controller: _amountController,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              hintText: "Amount",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              contentPadding: EdgeInsets.symmetric(
+                                vertical: kDefalutPadding,
+                                horizontal: 20,
                               ),
                             ),
                           ),
                         ],
                       ),
                     ),
-                  ],
+                  ),
                 ),
               ],
             ),
